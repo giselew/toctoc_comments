29fdea3722c9fbac970097dbf79259412d2162bc
giselew <giselew@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-02-01 20:29:27 +0000

    Cleanup repository with version 5.0.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/toctoc_comments/tags/4.3.2@83451 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_autoload.php
@@ -0,0 +1,9 @@
+<?php
+$extensionClassesPath = t3lib_extMgm::extPath('toctoc_comments') . '';
+$default = array(
+	'ux_tx_felogin_pi1' => $extensionClassesPath . 'class.ux_tx_felogin_pi1.php',
+);
+
+return $default;
+
+?>
